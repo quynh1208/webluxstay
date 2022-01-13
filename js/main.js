72cda69fcb123366a444b5bbf9f93de14e6831c0
quynh <90196532+code-ngu-zl@users.noreply.github.com>
2022-01-13 09:48:33 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,5 @@
+function show_amount() {
+    var hidden = document.getElementById('quantily');
+    if (hidden.style.display === 'none') hidden.style.display = 'block';
+    else hidden.style.display = 'none';
+}
